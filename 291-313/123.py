d149e29017ec93772921860edc26d906f6876f17
Jack <jackli6428@gmail.com>
2023-07-23 19:24:21 +0800

    change dir

--- 291-313/123.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-@author: AUTHOR
-@file: 123
-@time: 2023/07/12 10:29
-@project: huawei-od-python
-@desc: DESCRIPTION
-"""
--- 291-313/292_Max_salary.py
+++ 291-313/292_Max_salary.py
@@ -8,16 +8,13 @@
 @desc: 292_Max_salary
 @tags: Dynamic-Programming
 """
-def sulotion(T, n, tw)->int:
-	"""
-	T: Working Hours
-	n: Number of jobs
-	tw: [[Hour, Wage],...]
-	Return: Max Wages earned
-	"""
-	res = 0
-
-	return
-
-if __name__ == '__main__':
-	# assert solution( )
+def solution(t, n, jobs):
+	m_t = min(jobs, key = lambda x: x[0])[0] # min hours of jobs
+	dp = [[0] * (t + 1) for _ in range(n + 1)]
+	for i in range(1, n + 1):
+		for j in range(m_t, t + 1):
+			job = jobs[i - 1]
+			v = dp[i - 1][j]
+			new_v = job[1] + dp[i - 1][j - job[0]] if job[0] <= j else 0
+			dp[i][j] = max(v, new_v)
+    # return dp[n][t]
